076c132ba47033ae8ef24c072c45d36d28d9a734
Fernando Paredes <nano@fdp.io>
2017-01-02 07:30:04 -0800

    Return usage on invocation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+extern crate getopts;
+extern crate rustc_serialize;
+
+use getopts::Options;
+use std::env;
+
+fn print_usage(program: &str, opts: Options) {
+    println!("{}", opts.usage(&format!("Usage: {} [options] <data-path> <city>", program)));
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let program = &args[0];
+
+    let mut opts = Options::new();
+    opts.optflag("h", "help", "Show this usage message");
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m },
+        Err(e) => { panic!(e.to_string())},
+    };
+
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
+
+    let data_path = &matches.free[0];
+    let city: &str = &matches.free[1];
 }
